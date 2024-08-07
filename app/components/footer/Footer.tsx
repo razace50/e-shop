01404b5f37b6851a6a0c3da97bfa878cf12b2c41
Rajesh Sunar <134181496+razace50@users.noreply.github.com>
2024-08-07 12:47:08 +0545

    footer and navbar

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,5 +1,67 @@
+import Link from "next/link";
+import FooterList from "./FooterList";
+import Container from "../Container";
+import { MdFacebook } from "react-icons/md";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { FaGithub } from "react-icons/fa";
+
+
 const Footer = () => {
-    return ( <div>Footer</div> );
-}
- 
+    return (
+    <footer className=" bg-slate-700 text-slate-200 
+        text-sm mt-16">
+            <Container>
+            <div className="flex flex-col md:flex-row
+            justify-between pt-16 pb-8">
+                <FooterList>
+<h3 className="text-base font-bold mb-2">Shop Categories</h3>
+<Link href="#">Phones</Link>
+<Link href="#">Laptops</Link>
+<Link href="#">Desktops</Link>
+<Link href="#">Watches</Link>
+<Link href="#">Tvs</Link>
+<Link href="#">Accessories</Link>
+                </FooterList>
+                <FooterList>
+<h3 className="text-base font-bold mb-2">Customer Service</h3>
+<Link href="#">Contact Us</Link>
+<Link href="#">Shipping Policy</Link>
+<Link href="#">Returns & Exchanges</Link>
+<Link href="#">FAQs</Link>
+                </FooterList>
+                <div className="w-full md:w-1/3 nmb-6
+                md:mb-0">
+<h3 className="text-base font-bold mb-2">About Us</h3>
+<p className="mb-2">At our electronics
+    store, we are dedicated to provide the
+    latest and greatest devices and accessories
+    to our customers. With a wide selection of
+    Phones, TVs, Laptops, Watches, and
+    accessories
+    </p>
+    <p>&copy; {new Date().getFullYear()}.
+        E~Shop (Rajesh & Umesh). All right reserved
+
+    </p>
+                </div>
+                <FooterList>
+                    <h3 className="text-base font-bold mb-2">
+                        Follow Us</h3>
+                        <div className="flex gap-2">
+                        <Link href="#">
+                        <MdFacebook size={24} />
+                        </Link>
+                        <Link href="#">
+                        <AiFillTwitterCircle size={24} />
+                        </Link>
+                        <Link href="https://github.com/razace50">
+                        <FaGithub size={24} />
+                        </Link>
+                        </div>
+                </FooterList>
+            </div>
+            </Container>
+            </footer> );
+};
+
 export default Footer;
--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -1,5 +1,10 @@
+import Link from "next/link";
 import Container from "../Container";
+import { Redressed } from "next/font/google";
 
+const redressed = Redressed({subsets: ['latin'],
+    weight: ['400']
+});
 const NavBar = () => {
     return ( <div
     className="
@@ -12,17 +17,26 @@ const NavBar = () => {
     ">
         <div className="py-4 border-b-[1px]">
 <Container>
-    <div>
-        <link href="/">E-Shop</link>
-        <div>Search</div>
-        <div>
+    <div className="
+    flex
+    items-center
+    justify-between
+    gap-3
+    md:gap-0
+    ">
+        <Link href="/" className={`${redressed.className}
+        font-bold text-2xl`}>E-Shop</Link>
+        <div className="hidden md:block">Search</div>
+        <div className="flex items-center gap-8
+        md:gap-12">
             <div>CartCount</div>
+            <div>UserMenu</div>
         </div>
     </div>
 </Container>
         </div>
-    </div> 
+    </div>
     );
-};
+}
 
 export default NavBar;

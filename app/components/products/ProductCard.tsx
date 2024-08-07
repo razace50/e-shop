cf1946c4f2c031b3ac9f6e41ebfcd0f157b2c5d3
Rajesh Sunar <134181496+razace50@users.noreply.github.com>
2024-08-07 22:12:02 +0545

    card product

---
+++ app/components/products/ProductCard.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+const ProductCard = () => {
+    return ( <div className="col-span-1
+    cursor-pointer
+    border-[1.2px]
+    border-slate-200
+    border-slate-50
+    rounded-sm
+    p-2
+    transition
+    hover:scale-105
+    text-center
+    text-sm
+    ">
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div> );
+};
+ 
+export default ProductCard;

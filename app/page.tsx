7f7f23a8ac82d2d44b09d64bbd0f6207895890a6
Rajesh Sunar <134181496+razace50@users.noreply.github.com>
2024-08-07 21:31:32 +0545

    list product truncated

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
+import { products } from "@/utils/products"
 import Container from "./components/Container"
 import HomeBanner from "./components/HomeBanner"
+import { truncateText } from "@/utils/truncateText";
 export default function Home() {
   return (
     <div className="p-8">
       <Container>
       <div>
         <HomeBanner/>
+      </div>
+      <div className="grid grid-cols-2
+      sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5
+      2xl:grid-cols-6 gap:8">
+        {products.map((product: any) => {
+          return <div key={product.id}>
+            {truncateText(product.name)}
+          </div>;
+        })}
       </div>
       </Container>
     </div>

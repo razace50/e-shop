28341c8ebb5c2f421267521a59e5db0713b1b168
Rajesh Sunar <134181496+razace50@users.noreply.github.com>
2024-08-18 21:14:33 +0545

    ..

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,10 +1,11 @@
 import { Children } from "react";
-interface ContainerProps{
-    children: React.ReactNode
+interface ContainerProps {
+  children: React.ReactNode;
 }
-const Container: React.FC<ContainerProps> = ({children}) => {
-    return ( <div
-    className="
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return (
+    <div
+      className="
     max-w-[1920px]
     mx-auto
     xl:px-20
@@ -12,8 +13,9 @@ const Container: React.FC<ContainerProps> = ({children}) => {
     px-4
     "
     >
-        {children}
-    </div> );
-}
- 
+      {children}
+    </div>
+  );
+};
+
 export default Container;

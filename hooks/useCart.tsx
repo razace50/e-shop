fa5cb5b3d17b7c8ee19bd2909ec97948d573a927
Rajesh Sunar <134181496+razace50@users.noreply.github.com>
2024-09-24 19:41:26 +0545

    persist cart

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -1,5 +1,11 @@
 import { CartProductType } from "@/app/product/[productId]/ProductDetails";
-import { createContext, useCallback, useContext, useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 type CartContextType = {
   cartTotalQty: number;
@@ -17,13 +23,29 @@ export const CartContextProvider = (props: Props) => {
   const [cartTotalQty, setCartTotalQty] = useState(0);
   const [cartProducts, setCartProducts] = useState<CartProductType[]>([]);
 
+  useEffect(() => {
+    const cartItems: string | null = localStorage.getItem("eShopCartItems");
+    const cProducts: CartProductType[] = cartItems ? JSON.parse(cartItems) : [];
+
+    if (cProducts.length > 0) {
+      setCartProducts(cProducts);
+
+      // Update the total quantity based on the products in the cart
+      const totalQty = cProducts.reduce(
+        (acc, product) => acc + product.quantity,
+        0
+      );
+      setCartTotalQty(totalQty);
+    }
+  }, []);
+
   const handleAddProductToCart = useCallback((product: CartProductType) => {
     setCartProducts((prev) => {
       const updatedCart = [...prev, product];
 
       // Update the total quantity
       setCartTotalQty((prevQty) => prevQty + product.quantity);
-
+      localStorage.setItem("eShopCartItems", JSON.stringify(updatedCart));
       return updatedCart;
     });
   }, []);

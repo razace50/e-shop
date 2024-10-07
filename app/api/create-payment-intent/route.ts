0d5711cbb5d280a3ac9bce98480c6ea6482b4e4a
Rajesh Sunar <134181496+razace50@users.noreply.github.com>
2024-10-07 19:18:13 +0545

    payment intent

---
+++ app/api/create-payment-intent/route.ts
@@ -0,0 +1,26 @@
+import Stripe from "stripe";
+import prisma from "@/libs/prismadb";
+import { NextResponse } from "next/server";
+import { CartProductType } from "@/app/product/[productId]/ProductDetails";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import { error } from "console";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2024-09-30.acacia",
+});
+
+const calculateOrderAmount = (items: CartProductType[]) => {
+  const totalPrice = items.reduce((acc, item) => {
+    const itemTotal = item.price * item.quantity;
+    return (acc = itemTotal);
+  }, 0);
+  return totalPrice;
+};
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+}
